d2e04ad9f7cd5e1916ca0ee4959fd06d100f84d5
Jorge David Iranzo Bartolome <jdiranzobartolome@gmail.com>
2021-06-17 16:24:46 +0200

    update our menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 import './pages/homepage/homepage.style.scss';
 
 import HomePage from './pages/homepage/homepage.component';
 
+const HatsPage = () => {
+  return(
+    <div>
+    <h1>HATS PAGE</h1>
+  </div>
+  )
+  
+}
+
 function App() {
   return (
-    <HomePage />
+    <Switch>
+      <Route exact path='/'component={HomePage} />
+      <Route  path='/shop/hats'component={HatsPage} />
+
+    </Switch>
   );
 }
 
