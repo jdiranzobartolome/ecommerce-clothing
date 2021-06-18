5d242702e093dd4655845735dbb5ed00e1f50acf
Jorge David Iranzo Bartolome <jdiranzobartolome@gmail.com>
2021-06-18 19:10:10 +0200

    updated shop data with new data, updated cart utils to group items in cart reducer

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 import './custom-button.style.scss';
 
-const CustomButton = ({ children, isGoogleSignIn, ...otherProps }) => (
+const CustomButton = ({ children, isGoogleSignIn, inverted, ...otherProps }) => (
     //The {...otherProps} comming from the original form is passing
     // things like type='input'...etc. Children means "anything that is 
     // whithin the parent. In this case, the words "Sign in".
     // For example, the "children" of this button, would be {children}.
-    <button className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`} 
+    <button className={`${inverted ? 'inverted' : ''} ${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`} 
     {...otherProps }>
         {children}
     </button>
---
+++ src/redux/cart/cart.utils.js
@@ -0,0 +1,18 @@
+// Since the original Items from the database (in our case, hardcoded into 
+// in son file) do not have a quantity parameter, we need to add that to count the number 
+// of each item in the cart. 
+export const addItemToCart = (cartItems, cartItemToAdd) => {
+    const existingCartItem = cartItems.find(
+        cartItem => cartItem.id === cartItemToAdd.id
+    );
+
+    if (existingCartItem) {
+        return cartItems.map(cartItem => 
+            cartItem.id === cartItemToAdd.id 
+            ? {...cartItem, quantity: cartItem.quantity + 1}
+            :  cartItem
+        )
+    } else {
+        return [...cartItems, {...cartItemToAdd, quantity: 1} ]
+    }
+}
